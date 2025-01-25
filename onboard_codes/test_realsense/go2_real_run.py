ff31b038474223f66effbf80e1b6401483b7e747
hanlinniu <niuhanlin@icloud.com>
2025-01-25 12:24:54 +0000

    update

--- onboard_codes/test_realsense/go2_real_run.py
+++ onboard_codes/test_realsense/go2_real_run.py
@@ -145,6 +145,8 @@ class Go2Node(UnitreeRos2Real):
             self.use_stand_policy = False
             self.use_parkour_policy = False
             self.use_sport_mode = True
+            self.yaw = torch.zeros(1, 2, device=self.device)  # Initialize yaw to zeros
+            self.depth_latent = torch.zeros(1, 32, device=self.device)  # Initialize depth_latent to zeros
             self._sport_state_change(1)
             self._sport_mode_change(ROBOT_SPORT_API_ID_BALANCESTAND)
 
